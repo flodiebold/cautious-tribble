d6e154e3f7eee716daddb5a6cfdaab2476334727
Florian Diebold <florian.diebold@freiheit.com>
2018-08-16 21:06:50 +0200

    ui: Add material UI

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-const ws = new WebSocket("ws://" + document.location.host + "/api");
-
-ws.onopen = ev => {};
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 interface DeployerStatus {
     deployed_version: string;
@@ -49,29 +48,70 @@ interface UiData {
     transitions: { [key: string]: TransitionStatus };
 }
 
-const uiData: UiData = {
-    counter: 0,
-    deployers: {},
-    transitions: {}
-};
+class Page extends React.Component<{}, { tab: number; data: UiData }> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            tab: 0,
+            data: {
+                counter: 0,
+                deployers: {},
+                transitions: {}
+            }
+        };
 
-ws.onmessage = ev => {
-    const data: Message = JSON.parse(ev.data);
+        const ws = new WebSocket("ws://" + document.location.host + "/api");
 
-    if (data.type === "FullStatus" || data.type === "DeployerStatus") {
-        Object.assign(uiData.deployers, data.deployers);
+        ws.onopen = ev => {};
+        ws.onmessage = this.handleWebSocketMessage;
     }
 
-    if (data.type === "FullStatus" || data.type === "TransitionStatus") {
-        Object.assign(uiData.transitions, data.transitions);
+    handleWebSocketMessage = (ev: MessageEvent) => {
+        const message: Message = JSON.parse(ev.data);
+
+        this.setState(state => {
+            const data = state.data;
+            if (
+                message.type === "FullStatus" ||
+                message.type === "DeployerStatus"
+            ) {
+                Object.assign(data.deployers, message.deployers);
+            }
+
+            if (
+                message.type === "FullStatus" ||
+                message.type === "TransitionStatus"
+            ) {
+                Object.assign(data.transitions, message.transitions);
+            }
+
+            data.counter = message.counter;
+            return { data };
+        });
+    };
+
+    handleTabChange = (ev: any, tab: number) => {
+        this.setState({ tab });
+    };
+
+    render() {
+        return (
+            <div>
+                <AppBar position="static">
+                    <Tabs
+                        value={this.state.tab}
+                        onChange={this.handleTabChange}
+                    >
+                        <Tab label="Stuff" />
+                        <Tab label="Data" />
+                    </Tabs>
+                </AppBar>
+                {this.state.tab === 1 && (
+                    <pre>{JSON.stringify(this.state.data, null, 4)}</pre>
+                )}
+            </div>
+        );
     }
+}
 
-    uiData.counter = data.counter;
-
-    ReactDOM.render(
-        <pre>{JSON.stringify(uiData, null, 4)}</pre>,
-        document.getElementById("main")
-    );
-};
-
-ReactDOM.render(<div />, document.getElementById("main"));
+ReactDOM.render(<Page />, document.getElementById("main"));
