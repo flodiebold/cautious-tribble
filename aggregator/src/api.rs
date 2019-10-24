6b6a842d351ddab5e67193f11325193c2111b0c6
Florian Diebold <florian.diebold@freiheit.com>
2019-10-24 21:40:36 +0200

    Various fixes and upgrades

--- aggregator/src/api.rs
+++ aggregator/src/api.rs
@@ -2,11 +2,13 @@ use std::sync::{atomic::Ordering, Arc};
 use std::thread;
 
 use failure::Error;
-use futures::{future, sync::mpsc};
+use futures::{channel::mpsc, future};
+use futures_util::{FutureExt, SinkExt, StreamExt, TryStreamExt};
 use log::{debug, info, trace};
 use serde_derive::Deserialize;
 use serde_json::json;
-use warp::{self, Filter, Future, Rejection, Sink, Stream};
+use tokio::runtime::Runtime;
+use warp::{self, ws::WebSocket, Filter, Future, Rejection};
 
 use common::aggregator::{EnvName, Message, ResourceId};
 use common::repo::Id;
@@ -17,103 +19,37 @@ fn health(_state: Arc<ServiceState>) -> impl warp::Reply {
     warp::reply::json(&json!({}))
 }
 
-fn deploy(state: Arc<ServiceState>, body: DeploymentData) -> Result<impl warp::Reply, Rejection> {
+#[derive(Debug)]
+struct DeployError(failure::Error);
+impl warp::reject::Reject for DeployError {}
+
+async fn deploy(
+    state: Arc<ServiceState>,
+    body: DeploymentData,
+) -> Result<impl warp::Reply, Rejection> {
     info!("deploy {:?}", body);
     // TODO this should be done by another thread...
     // TODO return commit ID
-    let _result_commit = do_deploy(state, body).map_err(|e| warp::reject::custom(e.compat()))?;
+    let _result_commit =
+        do_deploy(state, body).map_err(|e| warp::reject::custom(DeployError(e)))?;
     Ok(warp::reply())
 }
 
 pub fn start(service_state: Arc<ServiceState>) -> thread::JoinHandle<()> {
     thread::spawn(move || {
+        let rt = Runtime::new().expect("Could not create runtime");
         let port = service_state.env.api_port.unwrap_or(9001);
         let service_state_1 = Arc::clone(&service_state);
         let state = warp::any().map(move || Arc::clone(&service_state_1));
         let health = warp::path("health")
             .and(warp::path::end())
-            .and(warp::get2())
+            .and(warp::get())
             .and(state.clone())
             .map(health);
         let service_state_1 = service_state.clone();
-        let ws_handler = warp::ws2().map(move |ws: warp::ws::Ws2| {
+        let ws_handler = warp::ws().map(move |ws: warp::ws::Ws| {
             let service_state = service_state_1.clone();
-            ws.on_upgrade(move |websocket| {
-                let (ws_tx, rx) = websocket.split();
-
-                // Create a channel to allow different threads to send messages on the websocket
-                let (tx, chan_rx) = mpsc::channel(20);
-
-                let client_id = service_state.client_counter.fetch_add(1, Ordering::SeqCst);
-
-                // Send messages from the channel to the websocket
-                warp::spawn(
-                    // hopefully we can use async/await soon...
-                    chan_rx
-                        .then(move |msg: Result<Message, ()>| {
-                            let text = match serde_json::to_string(
-                                &msg.expect("channel doesn't yield errors"),
-                            ) {
-                                Ok(t) => t,
-                                Err(err) => {
-                                    info!(
-                                        "Error sending WebSocket message to {}: {}",
-                                        client_id, err
-                                    );
-                                    return Ok(None);
-                                }
-                            };
-                            Ok(Some(warp::ws::Message::text(text)))
-                        })
-                        .take_while(|o| Ok(o.is_some()))
-                        .filter_map(|o| o)
-                        .map_err::<warp::Error, _>(|_e: ()| unreachable!())
-                        .forward(ws_tx)
-                        .map_err(move |err| {
-                            info!(
-                                "WebSocket sender for {} closed with error: {}",
-                                client_id, err
-                            );
-                        })
-                        .map(|_| ()),
-                );
-
-                // TODO add a counter query parameter
-                let full_status = service_state.full_status.read().unwrap().clone();
-                warp::spawn(
-                    tx.clone()
-                        .send(Message::FullStatus((*full_status).clone()))
-                        .map_err(move |err| {
-                            info!(
-                                "Could not send first message to WebSocket {}: {}",
-                                client_id, err
-                            );
-                        })
-                        .map(|_| ()),
-                );
-
-                {
-                    let mut receivers = service_state.receivers.write().unwrap();
-                    receivers.push((client_id, tx));
-                }
-
-                rx.for_each(move |msg| {
-                    trace!("Websocket message for {}: {:?}", client_id, msg);
-
-                    future::ok(())
-                })
-                .then(move |r| {
-                    if let Err(e) = r {
-                        info!("Websocket {} closed with error: {}", client_id, e);
-                    } else {
-                        debug!("Websocket {} closed normally", client_id);
-                    }
-                    // remove connection from the receivers
-                    let mut receivers = service_state.receivers.write().unwrap();
-                    receivers.retain(|(id, _)| *id != client_id);
-                    future::ok(())
-                })
-            })
+            ws.on_upgrade(move |websocket| user_connected(websocket, service_state))
         });
         let api = warp::path("api");
         let ws = api.and(warp::path::end()).and(ws_handler);
@@ -131,7 +67,95 @@ pub fn start(service_state: Arc<ServiceState>) -> thread::JoinHandle<()> {
                 .unwrap_or("/ui/dist".into()),
         );
         let routes = health.or(ws).or(deploy).or(ui);
-        warp::serve(routes).run(([0, 0, 0, 0], port));
+        rt.spawn(warp::serve(routes).run(([0, 0, 0, 0], port)));
+
+        rt.shutdown_on_idle();
+    })
+}
+
+fn user_connected(
+    websocket: WebSocket,
+    service_state: Arc<ServiceState>,
+) -> impl Future<Output = ()> {
+    let (mut ws_tx, rx) = websocket.split();
+
+    // Create a channel to allow different threads to send messages on the websocket
+    let (tx, mut chan_rx) = mpsc::channel(20);
+
+    let client_id = service_state.client_counter.fetch_add(1, Ordering::SeqCst);
+
+    // Send messages from the channel to the websocket
+    warp::spawn(
+        async move {
+            while let Some(msg) = chan_rx.next().await {
+                let text = match serde_json::to_string(&msg) {
+                    Ok(t) => t,
+                    Err(err) => {
+                        info!("Error sending WebSocket message to {}: {}", client_id, err);
+                        continue;
+                    }
+                };
+
+                if let Err(err) = ws_tx.send(warp::ws::Message::text(text)).await {
+                    info!(
+                        "WebSocket sender for {} closed with error: {}",
+                        client_id, err
+                    );
+                    break;
+                }
+            }
+        }, /*
+           // hopefully we can use async/await soon...
+           chan_rx
+               .then(move |msg: Result<Message, ()>| {
+                   Ok(Some(warp::ws::Message::text(text)))
+               })
+               .take_while(|o| Ok(o.is_some()))
+               .filter_map(|o| o)
+               .map_err::<warp::Error, _>(|_e: ()| unreachable!())
+               .forward(ws_tx)
+               .map_err(move |err| {
+               })
+               .map(|_| ()),
+                   */
+    );
+
+    // TODO add a counter query parameter
+    let full_status = service_state.full_status.read().unwrap().clone();
+    let tx2 = tx.clone();
+    warp::spawn(async move {
+        if let Err(err) = tx2
+            .clone()
+            .send(Message::FullStatus((*full_status).clone()))
+            .await
+        {
+            info!(
+                "Could not send first message to WebSocket {}: {}",
+                client_id, err
+            );
+        }
+    });
+
+    {
+        let mut receivers = service_state.receivers.write().unwrap();
+        receivers.push((client_id, tx));
+    }
+
+    rx.try_for_each(move |msg| {
+        trace!("Websocket message for {}: {:?}", client_id, msg);
+
+        future::ok(())
+    })
+    .then(move |r| {
+        if let Err(e) = r {
+            info!("Websocket {} closed with error: {}", client_id, e);
+        } else {
+            debug!("Websocket {} closed normally", client_id);
+        }
+        // remove connection from the receivers
+        let mut receivers = service_state.receivers.write().unwrap();
+        receivers.retain(|(id, _)| *id != client_id);
+        future::ready(())
     })
 }
 
